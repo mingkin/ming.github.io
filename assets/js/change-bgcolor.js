acc1493bdf7702755c5711b254b8445bac7d7f0b
king <wangmaming@126.com>
2019-03-27 19:01:31 +0800

    Update change-bgcolor.js

--- assets/js/change-bgcolor.js
+++ assets/js/change-bgcolor.js
@@ -4,7 +4,7 @@ var x = 90,
 var flagX = 0;
     flagY = 2;
 setInterval(function () {
- navBg.style.background = "linear-gradient(" + x +
+    navBg.style.background = "linear-gradient(" + x +
         "deg, #3AA17E 10%, #00537E " + y + "%)";
     if (flagX == 0) {
         x++;
